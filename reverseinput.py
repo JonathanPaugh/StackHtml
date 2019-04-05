ec96237c7b82452f100cf430fed923950ccbeea1
japetheape <33143001+zogdar@users.noreply.github.com>
2019-04-04 22:09:11 -0700

    Update reverseinput.py
    -Updated Header
    -Turned script into function "reverseText()"

--- reverseinput.py
+++ reverseinput.py
@@ -1,14 +1,25 @@
-# Name: Cw
-# Project: User inputs a string and flips it in reverse
+# Name: Cw & Jpeezy
+# Project: User inputs a string and flips it in reverse and more...?
+
+#FUNCTIONS#
+
+def reverseText(text):
+	textReturn = ""
+
+	# Create a decrementing for loop that starts from the full length - 1
+	for i in range((int(len(text) - 1) ),-1 ,-1): # Iterate in reverse through string
+		textReturn += text[i] # textrev = previous characters + new character
+
+	# Return the backwards text
+	return textReturn
+
 
 text = input('What would you like to reverse: ') # This is the string to be reversed taken from clipboard
 type(text) # User inputs a string to be reversed
-textrev = '' # Reverse string variable
 
 # Let's reverse THIS
 
-# Create a decrementing for loop that starts from the full length - 1
-for i in range((int(len(text) - 1) ),-1 ,-1): # Iterate in reverse through string
-    textrev += text[i] # textrev = previous character + new character
+textReversed = reverseText(text) # Reverse string variable
 
-print(textrev) # Print the reversed string in the terminal
+print(textReversed) # Print the reversed string in the terminal
+
